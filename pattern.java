b57a638025d986a27a6e8a683babc903e3c22a91
ramya2499 <61619217+ramya2499@users.noreply.github.com>
2020-03-21 18:10:56 +0530

    Add files via upload

---
+++ pattern.java
@@ -0,0 +1,21 @@
+package com.tgt.igniteplus;
+
+public class pattern {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+
+            for (int j = 5 - i; j > 1; j--) {
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+
+    }
+}
+
+
